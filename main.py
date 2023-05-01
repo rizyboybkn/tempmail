28d897bc542b0d3bb50b0a59159522d57228bc23
dgbkn <anandrambkn@gmail.com>
2023-05-02 00:51:01 +0530

    dev

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from typing import Union
 import requests
 import uvicorn
-
 from fastapi import FastAPI
 from fp.fp import FreeProxy
+import random
+
+import re
+url = 'https://free-proxy-list.net/'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'}
+source = str(requests.get(url, headers=headers, timeout=10).text)
+data = [list(filter(None, i))[0] for i in re.findall('<td class="hm">(.*?)</td>|<td>(.*?)</td>', source)]
+groupings = [dict(zip(['ip', 'port', 'code', 'using_anonymous'], data[i:i+4])) for i in range(0, len(data), 4)]
+final_groupings = [{'full_ip':"{ip}:{port}".format(**i)} for i in groupings]
+
+def check_proxy(px):
+    try:
+        requests.get("https://www.google.com/", proxies = {"https":  px}, timeout = 2)
+    except Exception as x:
+        print('--'+px + ' is dead: '+ x.__class__.__name__)
+        return False
+    return True
+
+
+# print(groupings)
 
 app = FastAPI()
 
@@ -15,7 +34,12 @@ def root():
 
 @app.get("/getMail")
 def read_root():
-    proxy = FreeProxy(elite=True,timeout=1).get()
+    # proxy = FreeProxy(country_id=['US', 'BR'], timeout=0.3, rand=True).get()
+    proxy = random.choice(groupings)
+    proxy = proxy['ip']  + ":" + proxy['port']
+    while check_proxy(proxy) == False:  
+          proxy = random.choice(groupings)
+          proxy = proxy['ip']  + ":" + proxy['port']
     print(proxy)
 
     headers = {
@@ -32,9 +56,10 @@ def read_root():
     }
 
     response = requests.post('https://web2.temp-mail.org/mailbox', headers=headers,
+                             timeout=10,
                              proxies={
                                  'http': proxy,
-                                 'https': proxy
+                                 'https': proxy,
                              },)
 
     return response.json()
